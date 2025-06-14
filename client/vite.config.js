0e17fe87eece87991d6292c996c0396113347a51
HieuITMHG <danghuuhieu038@gmail.com>
2025-06-14 10:53:26 +0700

    make realtime change dev env

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
 
 export default defineConfig({
+  server: {
+    host: true,
+    port: 5173, // hoặc cổng bạn đã mở
+    watch: {
+      usePolling: true, // BẮT BUỘC trong môi trường Docker
+    },
+  },
   plugins: [
     tailwindcss(),
     react()],
